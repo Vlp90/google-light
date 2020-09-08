9f6b986c6c6dac12baeec47282c2e0e6e7e15f57
Vlp90 <vladimir.leloup@gmail.com>
2020-09-08 10:29:21 +0200

    Mui setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import Home from "./pages/Home"
 
 function App() {
   return (
     <div className="app">
-      <h1>Hello</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import "./Home.css"
+
+function Home() {
+    return (
+        <div className="home">
+            <h1>Home</h1>
+
+            <div className="home__header">
+                <h1>About</h1>
+                <div className="home__headerLeft"></div>
+                <div className="home__headerRight"></div>
+            </div>
+            <div className="home__body">
+
+            </div>
+        </div>
+    )
+}
+
+export default Home
