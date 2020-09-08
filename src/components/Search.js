84189825479fd330afe9d23cc44b75f7899ddc51
Vlp90 <vladimir.leloup@gmail.com>
2020-09-08 11:54:42 +0200

    button enter key working and redirect to search page with history browser

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,29 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Search.css";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
 import { Button } from "@material-ui/core";
+import { useHistory } from "react-router-dom";
 
 function Search() {
+  const [input, setInput] = useState("");
+  const history = useHistory();
+  // after the search push to /search page
 
-    const search = e => {
-        // console.log(e)
-        e.preventDefault();
-    }
+  const search = (e) => {
+    // console.log(e)
+
+    // TO make the button enter working we change the tagg of the div by form
+    e.preventDefault();
+
+    history.push('/search')
+    console.log(input);
+  };
 
   return (
-    <div className="search">
+    <form className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
-        <input type="text" name="" id="" />
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          name=""
+          id=""
+        />
         <MicIcon className="search__inputIcon" />
       </div>
 
       <div className="search__buttons">
-        <Button onClick={search} variant="outlined">Google Search</Button>
+        <Button type="submit" onClick={search} variant="outlined">
+          Google Search
+        </Button>
         <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
-    </div>
+    </form>
   );
 }
 
