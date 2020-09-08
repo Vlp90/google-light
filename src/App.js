e9456c2dd6a9e102b249b9c39083f79c8513332e
Vlp90 <vladimir.leloup@gmail.com>
2020-09-08 10:33:50 +0200

    react router dom install

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./App.css";
 import Home from "./pages/Home"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import "./Home.css"
+import AppsIcon from '@material-ui/icons/Apps';
+import SearchIcon from '@material-ui/icons/Search';
+import MicIcon from '@material-ui/icons/Mic';
+// import Avatar from '@material-ui'
 
 function Home() {
     return (
@@ -9,7 +13,9 @@ function Home() {
             <div className="home__header">
                 <h1>About</h1>
                 <div className="home__headerLeft"></div>
-                <div className="home__headerRight"></div>
+                <div className="home__headerRight">
+
+                </div>
             </div>
             <div className="home__body">
 
