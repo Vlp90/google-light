6543be89b0261f08e89f5fd0013618a7488627c3
Vlp90 <vladimir.leloup@gmail.com>
2020-09-08 11:39:55 +0200

    buttons style ok

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,14 +2,26 @@ import React from "react";
 import "./Search.css";
 import SearchIcon from "@material-ui/icons/Search";
 import MicIcon from "@material-ui/icons/Mic";
+import { Button } from "@material-ui/core";
 
 function Search() {
+
+    const search = e => {
+        // console.log(e)
+        e.preventDefault();
+    }
+
   return (
     <div className="search">
       <div className="search__input">
         <SearchIcon className="search__inputIcon" />
         <input type="text" name="" id="" />
         <MicIcon className="search__inputIcon" />
+      </div>
+
+      <div className="search__buttons">
+        <Button onClick={search} variant="outlined">Google Search</Button>
+        <Button variant="outlined">I'm Feeling Lucky</Button>
       </div>
     </div>
   );
