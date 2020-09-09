96570724c1780e9c6b6eb9f7b5700037c9471fad
Vlp90 <vladimir.leloup@gmail.com>
2020-09-09 12:14:02 +0200

    add console-network-response from api to have layer

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,19 +1,24 @@
 import React from "react";
 import "./SearchPage.css";
-import {useStateValue} from "../StateProvider"
+import { useStateValue } from "../StateProvider";
+import useGoogleSearch from "../useGoogleSearch";
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
+  const { data } = useGoogleSearch(term);
 
   // custom service google Api
   // https://developers.google.com/custom-search/v1/using_rest
   // https://cse.google.com/cse/create/new
 
+  console.log(data)
+ 
   return (
     <div className="searchPage">
-       <div className="searchPage__header">
-       <h1>{term}</h1>
-       </div>
+      <div className="searchPage__header">
+        <h1>{term}</h1>
+        {/* <h2>{data}</h2> */}
+      </div>
       <div className="searPage__results"></div>
     </div>
   );
