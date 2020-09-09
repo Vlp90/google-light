cc1e0b6e61de783ec549a3596edf9b57033ec9ee
Vlp90 <vladimir.leloup@gmail.com>
2020-09-09 13:02:11 +0200

    search bar result page first step styling, need other stuffs later

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -4,7 +4,13 @@ import { useStateValue } from "../StateProvider";
 import useGoogleSearch from "../useGoogleSearch";
 import Response from "../response.js";
 import { Link } from "react-router-dom";
-import Search from '../components/Search'
+import Search from "../components/Search";
+import SearchIcon from "@material-ui/icons/Search";
+import DescriptionIcon from "@material-ui/icons/Description";
+import ImageIcon from "@material-ui/icons/Image";
+import LocalOfferIcon from "@material-ui/icons/LocalOffer";
+import RoomIcon from "@material-ui/icons/Room";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
 
 function SearchPage() {
   const [{ term }, dispatch] = useStateValue();
@@ -22,7 +28,7 @@ function SearchPage() {
   return (
     <div className="searchPage">
       <div className="searchPage__header">
-        <h1>{term}</h1>
+        {/* <h1>{term}</h1> */}
         {/* <h2>{data}</h2> */}
 
         <Link to="/">
@@ -32,11 +38,59 @@ function SearchPage() {
             alt="Google Logo"
           />
         </Link>
-      <div className="searchPage__headerBody">
-        <Search hideButtons />
-      </div>
+
+        <div className="searchPage__headerBody">
+
+          <Search hideButtons />
+
+            <div className="searchPage__options">
+
+          <div className="searchPage__optionsLeft">
+
+          <div className="searchPage__option">
+              <SearchIcon />
+              <Link to="all">All</Link>
+            </div>
+
+            <div className="searchPage__option">
+              <DescriptionIcon />
+              <Link to="all">News</Link>
+            </div>
+
+            <div className="searchPage__option">
+              <ImageIcon />
+              <Link to="all">Images</Link>
+            </div>
+
+            <div className="searchPage__option">
+              <LocalOfferIcon />
+              <Link to="all">Shopping</Link>
+            </div>
+
+            <div className="searchPage__option">
+              <RoomIcon />
+              <Link to="all">Maps</Link>
+            </div>
+
+            <div className="searchPage__option">
+              <MoreVertIcon />
+              <Link to="all">More</Link>
+            </div>
+
+          </div>
+            <div className="searchPage__optionsRight">
+
+              <div className="searchPage__option">
+                <Link to="setting">Settings</Link>
+              </div>
+              <div className="searchPage__option">
+                <Link to="tools">Tools</Link>
+              </div>
+            </div>
       </div>
       {/* <div className="searPage__results"></div> */}
+        </div>
+    </div>
     </div>
   );
 }
